708e05e59c2aa2ca824ad05835b7cd545350b0ba
OfficialDini <168492551+OfficialDini@users.noreply.github.com>
2024-04-29 23:23:21 -0400

    Update main.py

--- main.py
+++ main.py
@@ -46,8 +46,7 @@ starter_encouragements = [
     "Go get some hot cocoa. Bet it will cheer you up."
 ]
 
-if "responding" not in db.keys():
-    db["responding"] = True
+
 
 def get_quote():
     response = requests.get("https://zenquotes.io/api/random")
